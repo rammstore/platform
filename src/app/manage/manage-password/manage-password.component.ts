3d143d31f01cbd60399cfd4bdcc926b5484dc4db
Alex <a.proskurin@myfeelix.de>
2019-09-18 20:02:50 +0300

    manage module initial

---
+++ src/app/manage/manage-password/manage-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-password',
+  templateUrl: './manage-password.component.html',
+  styleUrls: ['./manage-password.component.scss']
+})
+export class ManagePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
