7cb3d623527372b6093d9e36a51baf9b29462e9f
Alex <a.proskurin@myfeelix.de>
2019-09-26 01:15:30 +0300

    strategy details: symbols added

--- src/app/strategy/strategy-details/strategy-details-symbols/strategy-details-symbols.component.ts
+++ src/app/strategy/strategy-details/strategy-details-symbols/strategy-details-symbols.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Strategy } from '@app/models';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-strategy-details-symbols',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./strategy-details-symbols.component.scss']
 })
 export class StrategyDetailsSymbolsComponent implements OnInit {
+  strategy: Strategy;
+  chartOptions: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.parent.data.subscribe((data: object) => {
+      this.strategy = data['strategy'];
+      console.log(this.strategy);
+
+      this.chartOptions = {
+        title: {
+          text: ''
+        },
+        legend: {
+          enabled: false
+        },
+        tooltip: {
+          useHTML: true,
+          headerFormat: '',
+          pointFormatter: function () {
+            return `<div class="arearange-tooltip-header">${Highcharts.numberFormat((this.y), 2, '.')}%</div>`;
+          }
+        },
+        type: 'arearange',
+        series: [{
+          type: 'pie',
+          name: this.strategy.symbols,
+          data: [{
+            y: 100,
+            name: this.strategy.symbols
+          }]
+        }]
+      };
+
+      Highcharts.chart('symbolsChartContainer', this.chartOptions);
+    });
   }
 
 }
