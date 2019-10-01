20850ecb506f2040abb64108504452e1b6292565
Alex <a.proskurin@myfeelix.de>
2019-10-01 14:31:55 +0300

    strategy-details - investments - question

--- src/app/strategy/strategy-details/strategy-details.component.ts
+++ src/app/strategy/strategy-details/strategy-details.component.ts
@@ -14,7 +14,7 @@ import { map, takeUntil } from 'rxjs/internal/operators';
   templateUrl: './strategy-details.component.html',
   styleUrls: ['./strategy-details.component.scss']
 })
-export class StrategyDetailsComponent implements OnInit , OnDestroy {
+export class StrategyDetailsComponent implements OnInit, OnDestroy {
   // https://blog.strongbrew.io/rxjs-best-practices-in-angular/#avoiding-memory-leaks
   // here we will unsubscribe from all subscriptions
   destroy$ = new Subject();
@@ -24,10 +24,9 @@ export class StrategyDetailsComponent implements OnInit , OnDestroy {
   modalRef: BsModalRef;
   links: ContentTabLink[];
 
-  constructor(
-    private route: ActivatedRoute,
-    private modalService: BsModalService
-  ) { }
+  constructor(private route: ActivatedRoute,
+              private modalService: BsModalService) {
+  }
 
   ngOnInit(): void {
     this.route.data
@@ -36,14 +35,17 @@ export class StrategyDetailsComponent implements OnInit , OnDestroy {
         map((data: object) => data['strategy'])
       )
       .subscribe((strategy: Strategy) => {
-      this.strategy = strategy;
+        this.strategy = strategy;
 
-      this.links = [
-        new ContentTabLink('Доходность', '/strategies/details/' + this.strategy.id),
-        new ContentTabLink('Инструменты', '/strategies/details/' + this.strategy.id + '/symbols'),
-        new ContentTabLink('Инвестиции', '/strategies/details/' + this.strategy.id + '/investments')
-      ];
-    });
+        this.links = [
+          new ContentTabLink('Доходность', '/strategies/details/' + this.strategy.id),
+          new ContentTabLink('Инструменты', '/strategies/details/' + this.strategy.id + '/symbols')
+        ];
+
+        if (this.strategy.accountsCount > 0) {
+          this.links.push(new ContentTabLink('Инвестиции', '/strategies/details/' + this.strategy.id + '/investments'));
+        }
+      });
   }
 
   openFundDialog(): void {
