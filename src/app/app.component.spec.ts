f7ab6eaa5f34e5a2831761d864af19f01d1c6578
Alex <a.proskurin@myfeelix.de>
2019-10-03 09:32:09 +0300

    changed strategystore name to RAMM + favicon

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'strategystore'`, () => {
+  it(`should have as title 'RAMM'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('strategystore');
+    expect(app.title).toEqual('RAMM');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to strategystore!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to RAMM!');
   });
 });
