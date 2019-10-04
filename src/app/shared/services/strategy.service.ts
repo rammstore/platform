d72bc840307cd8de4ff72b944a341a37665303ac
Alex <a.proskurin@myfeelix.de>
2019-10-04 16:34:02 +0300

    strategy service added comments

--- src/app/shared/services/strategy.service.ts
+++ src/app/shared/services/strategy.service.ts
@@ -16,15 +16,17 @@ class StrategiesSearchOptions {
   providedIn: 'root'
 })
 export class StrategyService {
+  // Активные стратегии
   activeStrategiesSubject: BehaviorSubject<Strategy[]> = new BehaviorSubject<Strategy[]>([]);
+  // Закрытые стратегии
   closedStrategiesSubject: BehaviorSubject<Strategy[]> = new BehaviorSubject<Strategy[]>([]);
-  strategiesSubject: BehaviorSubject<Strategy[]> = new BehaviorSubject<Strategy[]>([]);
 
   constructor(
     private http: HttpClient,
     private createInstanceService: CreateInstanceService
   ) { }
 
+  // Получение списка активных стратегий
   getActive(pagination?: Paginator): Observable<Strategy[]> {
     const options: StrategiesSearchOptions = new StrategiesSearchOptions();
     options.Filter = { IsActive: true };
@@ -54,6 +56,7 @@ export class StrategyService {
     return this.activeStrategiesSubject.asObservable();
   }
 
+  // Получение списка закрытых стратегий
   getClosed(pagination?: Paginator): Observable<Strategy[]> {
     const options: StrategiesSearchOptions = new StrategiesSearchOptions();
     options.Filter = { IsActive: false };
@@ -83,25 +86,7 @@ export class StrategyService {
     return this.closedStrategiesSubject.asObservable();
   }
 
-  getAll(page: number = 1): Observable<Strategy[]> {
-    const options: object = {
-      Filter: { IsActive: false },
-      Pagination: { CurrentPage: page }
-    };
-
-    this.http.post(`${CONFIG.baseApiUrl}/myStrategies.search`, options).subscribe((response: any) => {
-      const strategies: Strategy[] = [];
-
-      response.Strategies.forEach((s: any) => {
-        strategies.push(this.createInstanceService.createStrategy(s));
-      });
-
-      this.strategiesSubject.next(strategies);
-    });
-
-    return this.strategiesSubject.asObservable();
-  }
-
+  // Получение конкретной стратегии
   get(id: number): Observable<Strategy> {
     const options: object = {
       Filter: { ID: id }
@@ -112,6 +97,7 @@ export class StrategyService {
     }));
   }
 
+  // Получение графика для стратегий
   getChart(chartOptions: ChartOptions): Observable<any> {
     const options: any = {
       StrategyID: chartOptions.strategyID,
@@ -123,30 +109,29 @@ export class StrategyService {
     return this.http.post(`${CONFIG.baseApiUrl}/charts.get`, options);
   }
 
+  // Создание новой стратегии
   add(strategy: object) {
     return this.http.post(`${CONFIG.baseApiUrl}/myStrategies.add`, strategy).pipe(map((response: any) => {
       console.log(response);
     }));
   }
 
-  fund(accountID: number, amount: number) {
-    return this.http.post(`${CONFIG.baseApiUrl}/accounts.fund`, {AccountID: accountID, Amount: amount}).pipe(map((response: any) => {
-      console.log(response);
-    }));
-  }
-
+  // Постановка стратегии на паузу
   pause(strategyId: number): Observable<any> {
     return this.http.post(`${CONFIG.baseApiUrl}/myStrategies.pause`, {StrategyID: strategyId});
   }
 
+  // Возобновление стратегии
   resume(strategyId: number): Observable<any> {
     return this.http.post(`${CONFIG.baseApiUrl}/myStrategies.resume`, {StrategyID: strategyId});
   }
 
+  // Закрытие стратегии
   close(strategyId: number): Observable<any> {
     return this.http.post(`${CONFIG.baseApiUrl}/myStrategies.close`, {StrategyID: strategyId});
   }
 
+  // Инвестировать в стратегию (Создать инвестицию)
   invest(id: number, data: object): Observable<any> {
     const options: any = {
       StrategyID: id,
