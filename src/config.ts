6596f0e2bad6d3ee7bb102222291084029a352c7
Alex <a.proskurin@myfeelix.de>
2019-10-15 13:53:22 +0300

    api url fix

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const CONFIG = {
-  baseApiUrl: 'https://apitranslator20191015013613.azurewebsites.net'
+  baseApiUrl: 'https://apitranslator20191015013613.azurewebsites.net/api/client/v1'
 };
