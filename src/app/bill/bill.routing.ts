d108c565ecef88e49d9ed8346e9a112d9f61fe40
Alexey Proskurin <proskurin.sobaka@gmail.com>
2019-11-05 14:01:49 +0300

    last transfers mobile template fix

--- src/app/bill/bill.routing.ts
+++ src/app/bill/bill.routing.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BillComponent } from './bill.component';
-import { BillResultsComponent } from './bill-results/bill-results.component';
-import { BillLastTransfersComponent } from './bill-last-transfers/bill-last-transfers.component';
 
 const routes: Routes = [
-  { path: '', component: BillComponent, children: [
-    { path: '', component: BillResultsComponent },
-    { path: 'last-transfers', component: BillLastTransfersComponent }
-  ] }
+  { path: '', component: BillComponent }
 ];
 
 @NgModule({
--- src/app/shared/components/mobile-data-view/mobile-data-view.module.ts
+++ src/app/shared/components/mobile-data-view/mobile-data-view.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MobileDataViewInvestmentComponent } from './mobile-data-view-investment/mobile-data-view-investment.component';
 import { MobileDataViewStrategyComponent } from './mobile-data-view-strategy/mobile-data-view-strategy.component';
 import { MobileDataViewRatingComponent } from './mobile-data-view-rating/mobile-data-view-rating.component';
+import { MobileDataViewLastTransfersComponent } from './mobile-data-view-last-transfers/mobile-data-view-last-transfers.component';
 import { SharedModule } from '@app/shared.module';
 import { DataTableModule } from '@app/components/data-table/data-table.module';
 
@@ -9,7 +10,8 @@ import { DataTableModule } from '@app/components/data-table/data-table.module';
   declarations: [
     MobileDataViewInvestmentComponent,
     MobileDataViewStrategyComponent,
-    MobileDataViewRatingComponent
+    MobileDataViewRatingComponent,
+    MobileDataViewLastTransfersComponent
   ],
   imports: [
     SharedModule,
@@ -18,7 +20,8 @@ import { DataTableModule } from '@app/components/data-table/data-table.module';
   exports: [
     MobileDataViewInvestmentComponent,
     MobileDataViewStrategyComponent,
-    MobileDataViewRatingComponent
+    MobileDataViewRatingComponent,
+    MobileDataViewLastTransfersComponent,
   ]
 })
 export class MobileDataViewModule { }
