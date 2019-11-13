d2ca8c023d33947e52d7c082e94d92f4686299cd
Alexey Proskurin <proskurin.sobaka@gmail.com>
2019-11-13 11:09:14 +0300

    strategy add template fix

--- src/app/strategy/strategy-add/strategy-add.component.ts
+++ src/app/strategy/strategy-add/strategy-add.component.ts
@@ -41,7 +41,7 @@ export class StrategyAddComponent implements OnInit, OnDestroy {
 
   buildFormStep1(): void {
     this.formStep1 = this.fb.group({
-      name: ['', Validators.required],
+      name: ['', [Validators.required, Validators.pattern('^[0-9a-zA-Z_!,.? ]*$')]],
       fee: [25, [Validators.min(0), Validators.max(50)]],
       commission: [0, [Validators.min(0), Validators.max(100)]],
       isShared: [true]
