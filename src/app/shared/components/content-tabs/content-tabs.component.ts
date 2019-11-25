64847450f1328c7ca91a866370f6acd0d0399660
Alexey Proskurin <proskurin.sobaka@gmail.com>
2019-11-25 14:40:34 +0300

    strategy details link highlight fix (hack)

--- src/app/shared/components/content-tabs/content-tabs.component.ts
+++ src/app/shared/components/content-tabs/content-tabs.component.ts
@@ -14,7 +14,27 @@ export class ContentTabsComponent {
     private router: Router
   ) {}
 
-  isLinkActive(url: string): boolean {
-    return this.router.url === url;
+  isLinkActive(url: string, link?: ContentTabLink): boolean {
+    console.log(url);
+    console.log(this.router.url);
+    console.log(this.links);
+    console.log('-------------');
+    let result: boolean = false;
+
+    if (this.isStrategyInvestmentsClosedCheck(url, link)) {
+      result = true;
+    } else {
+      result = this.router.url === url;
+    }
+
+    return result;
+  }
+
+  isStrategyInvestmentsClosedCheck(url: string, link: ContentTabLink): boolean {
+    if (this.router.url.includes('strategies/details') && this.router.url.includes('investments/closed') && url.includes('strategies/details') && url.includes('investments') && link.name === 'Инвестиции') {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
