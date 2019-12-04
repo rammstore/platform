097ee9f7abec4ec49e996bc4c8e7b85dac269bad
Alexey Proskurin <proskurin.sobaka@gmail.com>
2019-12-04 11:15:07 +0300

    footer googlePlay img fix

--- src/app/main/main-footer/main-footer.component.ts
+++ src/app/main/main-footer/main-footer.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-main-footer',
   templateUrl: './main-footer.component.html',
   styleUrls: ['./main-footer.component.scss']
 })
-export class MainFooterComponent {}
+export class MainFooterComponent implements OnInit {
+  language: string;
+
+  constructor(
+    private translateService: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    this.language = this.translateService.currentLang;
+  }
+}
