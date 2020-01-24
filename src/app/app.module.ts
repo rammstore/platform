3c86ebb9c8c1a4d57e2ba6b81019dfd18104e926
Alexey Proskurin <proskurin.sobaka@gmail.com>
2020-01-24 11:07:41 +0300

    added custom translates files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,17 +13,24 @@ import { WalletService } from '@app/services/wallet.service';
 import { CommandService } from '@app/services/command.service';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { DataService } from "@app/services/data.service";
+import { DataService } from '@app/services/data.service';
 import { LoaderService } from '@app/services/loader.service';
 import { NotificationsService } from '@app/services/notifications.service';
 import localeRu from '@angular/common/locales/ru';
 import { registerLocaleData } from '@angular/common';
+import {MultiTranslateHttpLoader} from 'ngx-translate-multi-http-loader';
 
 registerLocaleData(localeRu, 'ru');
 
 // AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http);
+// }
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new MultiTranslateHttpLoader(httpClient, [
+    { prefix: './assets/i18n/', suffix: '.json' },
+    { prefix: './assets/i18n/custom/', suffix: '.json' },
+  ]);
 }
 
 @NgModule({
@@ -53,8 +60,9 @@ export function HttpLoaderFactory(http: HttpClient) {
     DataService,
     LoaderService,
     NotificationsService,
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
