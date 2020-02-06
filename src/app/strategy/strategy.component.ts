1a5834088dc309c117fe3d37948d1ed03145bcac
Alexey Proskurin <proskurin.sobaka@gmail.com>
2020-02-06 16:45:43 +0300

    external strategy creation

--- src/app/strategy/strategy.component.ts
+++ src/app/strategy/strategy.component.ts
@@ -1,26 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ContentTabLink } from '@app/components/content-tabs/content-tab-link';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap';
 import { StrategyAddComponent } from './strategy-add/strategy-add.component';
 import { Router } from '@angular/router';
+import { BrandService } from '@app/services/brand.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-strategy',
   templateUrl: './strategy.component.html',
   styleUrls: ['./strategy.component.scss']
 })
-export class StrategyComponent {
+export class StrategyComponent implements OnInit, OnDestroy {
+  // https://blog.strongbrew.io/rxjs-best-practices-in-angular/#avoiding-memory-leaks
+  // here we will unsubscribe from all subscriptions
+  destroy$ = new Subject();
+
   links: ContentTabLink[] = [
     new ContentTabLink('Активные', '/strategies'),
     new ContentTabLink('Закрытые', '/strategies/closed')
   ];
   modalRef: BsModalRef;
   methodArgs: any;
+  functionality: object;
 
   constructor(
     private modalService: BsModalService,
+    private brandService: BrandService,
     private router: Router
   ) { }
+
+  ngOnInit(): void {
+    this.brandService.functionality
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((f: object) => {
+        this.functionality = f;
+      });
+  }
 
   onRouterOutletActivate(component: any) {
       this.methodArgs = { paginator: component.paginator };
@@ -33,5 +50,9 @@ export class StrategyComponent {
       methodArgs: this.methodArgs
     };
     this.modalRef = this.modalService.show(StrategyAddComponent, options);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
