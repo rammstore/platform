0dbabb78b79beb4972566a9874361c3cb33ca9cd
Alexey Proskurin <proskurin.sobaka@gmail.com>
2020-02-20 10:52:15 +0300

    redirect fix

--- src/app/main/main.routing.ts
+++ src/app/main/main.routing.ts
@@ -3,20 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent, children: [
-    { path: '', redirectTo: 'account' },
-    { path: 'account', loadChildren: '../account/account.module#AccountModule' },
-    { path: 'strategies', loadChildren: '../strategy/strategy.module#StrategyModule' },
-    { path: 'manage', loadChildren: '../manage/manage.module#ManageModule' },
-    { path: 'spec', loadChildren: '../specification/specification.module#SpecificationModule' },
-    { path: 'help', loadChildren: '../help/help.module#HelpModule' },
-    { path: 'investments', loadChildren: '../investments/investments.module#InvestmentsModule' },
-    { path: 'rating', loadChildren: '../rating/rating.module#RatingModule' }
-  ] }
+  {
+    path: '', component: MainComponent, children: [
+      {path: '', redirectTo: 'account'},
+      {path: 'account', loadChildren: '../account/account.module#AccountModule'},
+      {path: 'strategies', loadChildren: '../strategy/strategy.module#StrategyModule'},
+      {path: 'manage', loadChildren: '../manage/manage.module#ManageModule'},
+      {path: 'spec', loadChildren: '../specification/specification.module#SpecificationModule'},
+      {path: 'help', loadChildren: '../help/help.module#HelpModule'},
+      {path: 'investments', loadChildren: '../investments/investments.module#InvestmentsModule'},
+      {path: 'rating', loadChildren: '../rating/rating.module#RatingModule'},
+      {path: '**', redirectTo: 'account'}
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {
+}
