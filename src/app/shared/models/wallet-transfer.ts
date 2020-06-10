8006eed25cbf910ba77ebb8e691a89a9a22a474e
Waver85 <konstantin.zakharov@ramm.tech>
2020-06-10 19:12:14 +0300

    added span translate before switch

--- src/app/shared/models/wallet-transfer.ts
+++ src/app/shared/models/wallet-transfer.ts
@@ -35,7 +35,7 @@ export class WalletTransfer {
   }
 
   getType(): string {
-    switch (this.type) {
+    <span translate> switch (this.type) {
       case 0:
         return 'Fund';
       case 1:
@@ -54,7 +54,7 @@ export class WalletTransfer {
         return 'Commission from';
       case 8:
         return 'Partners';
-    }
+    } </span>
   }
 
   isExternal(): boolean {
