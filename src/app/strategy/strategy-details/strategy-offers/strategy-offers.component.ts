c758d40d75f84305eea3f6bf20c9ca5507b2abb1
Andrey Baranov <v-andreyba@k2it.ru>
2020-08-25 15:16:33 +0300

    change RAMM-829

--- src/app/strategy/strategy-details/strategy-offers/strategy-offers.component.ts
+++ src/app/strategy/strategy-details/strategy-offers/strategy-offers.component.ts
@@ -9,121 +9,132 @@ import {StrategyOfferCreateComponent} from "./strategy-offer-create/strategy-off
 import {NotificationsService} from "@app/services/notifications.service";
 import {TableHeaderRow} from "@app/models/table-header-row";
 import {CustomDatePipe} from "@app/pipes/custom-date.pipe";
-import { PercentPipe } from '@angular/common';
+import {PercentPipe} from '@angular/common';
 
 @Component({
-  selector: 'app-strategy-offers',
-  templateUrl: './strategy-offers.component.html',
-  styleUrls: ['./strategy-offers.component.scss']
+    selector: 'app-strategy-offers',
+    templateUrl: './strategy-offers.component.html',
+    styleUrls: ['./strategy-offers.component.scss']
 })
 export class StrategyOffersComponent implements OnInit {
-  destroy$ = new Subject();
-  args: any;
-  strategy: Strategy;
-  modalRef: BsModalRef;
+    destroy$ = new Subject();
+    args: any;
+    strategy: Strategy;
+    modalRef: BsModalRef;
 
 
-  // component data
-  offers: Offer[];
+    // component data
+    offers: Offer[];
 
-  // table settings
-  tableHeader: TableHeaderRow[] = [
-    new TableHeaderRow([
-      new TableColumn({property: 'ID', label: 'ID'}),
-      new TableColumn({property: 'DTCreated', label: 'common.table.label.dtCreate', pipe: {pipe: CustomDatePipe}}),
-      new TableColumn({property: 'link', label: 'common.table.label.link'}),
-      new TableColumn({property: 'commissionRate', label: 'common.table.label.commissionRate'}),
-      new TableColumn({property: 'FeeRate', label: 'investment.details.strategy.feeRate', pipe: {pipe: PercentPipe}}),
-      // new TableColumn({property: 'PartnerShareRate', label: 'common.table.label.partner'}),
-      new TableColumn({property: 'OfferStatus', label: 'common.table.label.status'}),
-      new TableColumn({property: 'IsPublic', label: 'common.table.label.offer.public'}),
-      new TableColumn({property: 'offerManage', label: 'common.table.label.manage'})
-    ]),
-  ];
+    // table settings
+    tableHeader: TableHeaderRow[] = [
+        new TableHeaderRow([
+            new TableColumn({property: 'ID', label: 'ID'}),
+            new TableColumn({
+                property: 'DTCreated',
+                label: 'common.table.label.dtCreate',
+                pipe: {pipe: CustomDatePipe}
+            }),
+            new TableColumn({property: 'link', label: 'common.table.label.link'}),
+            new TableColumn({property: 'commissionRate', label: 'common.table.label.commissionRate'}),
+            new TableColumn({
+                property: 'FeeRate',
+                label: 'investment.details.strategy.feeRate',
+                pipe: {pipe: PercentPipe}
+            }),
+            // new TableColumn({property: 'PartnerShareRate', label: 'common.table.label.partner'}),
+            new TableColumn({property: 'OfferStatus', label: 'common.table.label.status'}),
+            new TableColumn({property: 'IsPublic', label: 'common.table.label.offer.public'}),
+            new TableColumn({property: 'offerManage', label: 'common.table.label.manage'})
+        ]),
+    ];
 
-  totalFields: string[] = ['account.equity', 'accounts', 'account.intervalPnL', 'feePaid', 'feeToPay'];
-  paginator: Paginator = new Paginator({
-    perPage: 10,
-    currentPage: 1
-  });
-
-  constructor(
-    public dataService: DataService,
-    private modalService: BsModalService,
-    private notificationsService: NotificationsService,
-    private route: ActivatedRoute
-  ) {
-  }
-
-  ngOnInit() {
-    this.args = {
-      strategyId: this.route.parent.params['_value'].id,
-      paginator: this.paginator
-    };
-
-    this.getStrategy();
-    this.getOffers();
-  }
-
-  getOfferLink(link: string) {
-    return `${location.origin}/strategies/link/${link}`;
-  }
-
-  getStrategy() {
-    this.dataService.getStrategyByID(this.args)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((strategy: Strategy) => {
-        console.log('strategy', strategy);
-        this.strategy = strategy;
-      });
-  }
-
-  getOffers(): void {
-    this.dataService.getOffers(this.strategy.id)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((offers) => {
-        this.offers = offers;
-      });
-  }
-
-  makeNotPublic() {
-    this.dataService.setPublicOffer(this.strategy.id).subscribe((item) => {
-      this.strategy.publicOffer = null;
-      this.getOffers();
-      this.notificationsService.open('notify.strategy.offer.change');
-    });
-  }
-
-  createOffer() {
-    const options: ModalOptions = new ModalOptions();
-
-    options.initialState = {
-      strategy: this.strategy
-    };
-
-    this.modalRef = this.modalService.show(StrategyOfferCreateComponent, options);
-
-    (<StrategyOfferCreateComponent>this.modalRef.content).onClose.subscribe(result => {
-      if (result === true) {
-        this.dataService.getStrategyByID(this.args);
-        this.getOffers();
-        // when pressed Yes
-      } else if (result === false) {
-        // when pressed No
-      } else {
-        // When closing the modal without no or yes
-      }
+    totalFields: string[] = ['account.equity', 'accounts', 'account.intervalPnL', 'feePaid', 'feeToPay'];
+    paginator: Paginator = new Paginator({
+        perPage: 10,
+        currentPage: 1
     });
 
-  }
-
-  onRowManageClick(offer: any) {
-    if (offer instanceof Offer) {
-      this.dataService.setPublicOffer(this.strategy.id, offer.id).subscribe((item) => {
-        this.notificationsService.open('notify.strategy.offer.change');
-        this.dataService.getStrategyByID(this.args);
-        this.getOffers();
-      });
+    constructor(
+        public dataService: DataService,
+        private modalService: BsModalService,
+        private notificationsService: NotificationsService,
+        private route: ActivatedRoute
+    ) {
     }
-  }
+
+    ngOnInit() {
+        this.args = {
+            strategyId: this.route.parent.params['_value'].id,
+            paginator: this.paginator
+        };
+
+        this.getStrategy();
+        this.getOffers();
+    }
+
+    getOfferLink(link: string) {
+        return `${location.origin}/strategies/link/${link}`;
+    }
+
+    getStrategy() {
+        this.dataService.getStrategyByID(this.args)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((strategy: Strategy) => {
+                console.log('strategy', strategy);
+                this.strategy = strategy;
+            });
+    }
+
+    getOffers(): void {
+        this.dataService.getOffers(this.strategy.id)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((offers) => {
+                this.offers = offers;
+            });
+    }
+
+    onMakePublic(status: boolean, offer: any) {
+        let observer;
+        if (!status) {
+            observer = this.dataService.setPublicOffer(this.strategy.id);
+        }
+        if (offer instanceof Offer) {
+            observer = this.dataService.setPublicOffer(this.strategy.id, offer.id);
+        }
+
+        observer.subscribe((item) => {
+            this.notificationsService.open('notify.strategy.offer.change');
+            this.dataService.getStrategyByID(this.args)
+                .pipe(takeUntil(this.destroy$))
+                .subscribe((strategy: Strategy) => {
+                    console.log('strategy', strategy);
+                    this.strategy = strategy;
+                });
+            this.getOffers();
+        });
+    }
+
+    createOffer() {
+        const options: ModalOptions = new ModalOptions();
+
+        options.initialState = {
+            strategy: this.strategy
+        };
+
+        this.modalRef = this.modalService.show(StrategyOfferCreateComponent, options);
+
+        (<StrategyOfferCreateComponent>this.modalRef.content).onClose.subscribe(result => {
+            if (result === true) {
+                this.dataService.getStrategyByID(this.args);
+                this.getOffers();
+                // when pressed Yes
+            } else if (result === false) {
+                // when pressed No
+            } else {
+                // When closing the modal without no or yes
+            }
+        });
+
+    }
 }
