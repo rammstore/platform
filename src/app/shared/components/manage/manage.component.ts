8630f8cb0e56fda2710a720acf2623b8bdcf5e3c
Andrey Baranov <v-andreyba@k2it.ru>
2020-09-02 11:13:38 +0300

    change rating page on manage

--- src/app/shared/components/manage/manage.component.ts
+++ src/app/shared/components/manage/manage.component.ts
@@ -33,10 +33,6 @@ export class ManageComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    if (this.data.name === "Import001") {
-      console.log(this.data);
-    }
-
     switch (true) {
       case (this.data instanceof Strategy): {
         this.dataType = 'strategy';
@@ -57,7 +53,7 @@ export class ManageComponent implements OnInit {
     return this.section === SectionEnum.strategy && this.dataType === 'strategy' && this.data.isMyStrategy && this.data.account && this.data.account.id && !this.data.account.isSecurity;
   }
   get isRatingPage(): boolean {
-    return this.section === SectionEnum.rating && this.dataType === 'strategy' && this.data.isMyStrategy && this.data.account && this.data.account.id && !this.data.account.isSecurity;
+    return this.section === SectionEnum.rating && this.dataType === 'strategy' && this.data.account && this.data.account.id;
   }
   get isInvestOtherStrategy(): boolean {
     return this.dataType === 'account' && !this.data.isMyStrategy;
