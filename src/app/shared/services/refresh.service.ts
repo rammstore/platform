2f9b406ae53a9b90c2f552127e41b49ed660fd66
Vadym Bei <bey1705@gmail.com>
2020-10-15 13:00:49 +0300

    refresh.service.ts was added

---
+++ src/app/shared/services/refresh.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { IRefresh } from '@app/models/refresh.interface';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RefreshService {
+  private _refresh$: ReplaySubject<any> = new ReplaySubject<any>(null);
+
+  constructor() { }
+
+  get refresh$(){
+    return this._refresh$.asObservable();
+  }
+
+  set refresh(value: any){
+    this._refresh$.next(value);
+  }
+}
