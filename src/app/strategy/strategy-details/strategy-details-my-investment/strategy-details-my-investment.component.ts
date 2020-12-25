21823f9cbff7a6a56e467fb09b6192f02050aa77
Vadym Bei <bey1705@gmail.com>
2020-12-25 13:16:16 +0200

    update

--- src/app/strategy/strategy-details/strategy-details-my-investment/strategy-details-my-investment.component.ts
+++ src/app/strategy/strategy-details/strategy-details-my-investment/strategy-details-my-investment.component.ts
@@ -46,7 +46,6 @@ export class StrategyDetailsMyInvestmentComponent implements OnInit, OnDestroy {
     this.strategy = this.strategyService.strategy;
 
     if (!this.strategy) {
-      debugger
       this.getStrategyById(this.args)
         .pipe(takeUntil(this.destroy$))
         .subscribe((item) => {
@@ -56,7 +55,6 @@ export class StrategyDetailsMyInvestmentComponent implements OnInit, OnDestroy {
 
       this.getAccountStatement({ accountId: this.accountId });
     } else {
-      debugger
       this.offer$ = of(this.strategy.account.offer);
       this.getAccountStatement({ accountId: this.strategy.account.id });
     }
